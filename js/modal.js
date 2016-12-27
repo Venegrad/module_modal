9b7164c0d8f87378d8c1eab1fc306c581d76a394
venegrad <venegrad@gmail.com>
2016-12-27 08:01:27 +0200

    some bug fixed

--- js/modal.js
+++ js/modal.js
@@ -1,41 +1,35 @@
 function modalresizer() {
-  $(".modal-window").each(function () {
-    if ($(this).innerHeight() > $(window).height()) {
-      $(this).addClass("js-window-absolut");
-      var toppos = $(window).scrollTop();
-      $(this).css({
-        "top": toppos
-      });
-    } else {
-      $(this).removeClass("js-window-absolut");
-      var getheight = $(window).height();
-      var getmodalheight = $(this).innerHeight();
-      var gtx = getheight - getmodalheight;
-      gtx = gtx / 2;
-      $(this).css({
-        "top": gtx
-      });
-    }
-  });
+  $(".modal-window.js-active").removeClass("js-window-absolute");
+  $("body").removeClass("overflow-owt");
+  if ($(".modal-window.js-active").innerHeight() > ($(window).height() - ($(window).height() * 0.1))) {
+    $(".modal-window.js-active").addClass("js-window-absolute");
+    $("body").addClass("overflow-owt");
+  } else {
+    var getheight = $(window).height();
+    var getmodalheight = $(".modal-window.js-active").innerHeight();
+    var gtx = getheight - getmodalheight;
+    gtx = gtx / 2;
+    $(".modal-window.js-active").css({
+      "top": gtx
+    });
+  }
 }
 
 function openmodal(artic) {
-  modalresizer();
-
   if ($(".modal-window.js-active").length > 0) {
-    $(".modal-window").animate({
+    $(".modal-window.js-active").animate({
       opacity: 0
     }, 500, function () {
-      $(".modal-window").removeClass("js-active");
-
+      $(".modal-window.js-active").removeClass("js-active js-window-absolute");
       $("#" + artic).addClass("js-active");
+      modalresizer();
       $("#" + artic).animate({
         opacity: 1
       }, 500);
-
     });
   } else {
     $("#" + artic).addClass("js-active");
+    modalresizer();
     $("#" + artic).animate({
       opacity: 1
     }, 500);
@@ -46,22 +40,25 @@ function openmodal(artic) {
   }
 }
 
+function closemodal() {
+  $(".modal-window, .modal-shadow").animate({
+    opacity: 0
+  }, 500, function () {
+    $(".modal-window, .modal-shadow").removeClass("js-active");
+  });
+}
+
 $(document).ready(function () {
+  
   $("body").prepend("<div class='modal-shadow'></div>");
 
   $(".modal-window").each(function () {
     $(this).prepend("<a href='' class='closemodal'></a>");
-    $(this).addClass("js-window");
-    modalresizer();
   });
 
-  $("body").on("click", ".closemodal, .modal-shadow", function (event) {
-    $(".modal-window, .modal-shadow").animate({
-      opacity: 0
-    }, 500, function () {
-      $(".modal-window, .modal-shadow").removeClass("js-active");
-    });
-    event.preventDefault();
+  $("body").on("click", ".closemodal, .modal-shadow", function (e) {
+    closemodal();
+    e.preventDefault();
   });
   
   $(window).resize(function() {
