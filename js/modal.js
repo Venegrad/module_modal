3224b190b0f662ded58467a936380e5057e4016c
venegrad <venegrad@gmail.com>
2017-01-31 12:55:51 +0200

    change css

--- js/modal.js
+++ js/modal.js
@@ -1,39 +1,39 @@
 function modalresizer() {
-  $(".modal-window.js-active").removeClass("js-window-absolute");
-  $("body").removeClass("overflow-owt");
-  if ($(".modal-window.js-active").innerHeight() > ($(window).height() - ($(window).height() * 0.1))) {
-    $(".modal-window.js-active").addClass("js-window-absolute");
-    $("body").addClass("overflow-owt");
+  $(".modal-window.modal-window_active").removeClass("modal-window_absolute");
+  $("body").removeClass("modal-window_active");
+  if ($(".modal-window.modal-window_active").innerHeight() > ($(window).height() - ($(window).height() * 0.1))) {
+    $(".modal-window.modal-window_active").addClass("modal-window_absolute");
+    $("body").addClass("modal-window_active");
   } else {
     var getheight = $(window).height();
-    var getmodalheight = $(".modal-window.js-active").innerHeight();
+    var getmodalheight = $(".modal-window.modal-window_active").innerHeight();
     var gtx = getheight - getmodalheight;
     gtx = gtx / 2;
-    $(".modal-window.js-active").css({
+    $(".modal-window.modal-window_active").css({
       "top": gtx
     });
   }
 }
 
 function openmodal(artic) {
-  if ($(".modal-window.js-active").length > 0) {
-    $(".modal-window.js-active").animate({
+  if ($(".modal-window.modal-window_active").length > 0) {
+    $(".modal-window.modal-window_active").animate({
       opacity: 0
     }, 500, function () {
-      $(".modal-window.js-active").removeClass("js-active js-window-absolute");
-      $("#" + artic).addClass("js-active");
+      $(".modal-window.modal-window_active").removeClass("modal-window_active modal-window_absolute");
+      $("#" + artic).addClass("modal-window_active");
       modalresizer();
       $("#" + artic).animate({
         opacity: 1
       }, 500);
     });
   } else {
-    $("#" + artic).addClass("js-active");
+    $("#" + artic).addClass("modal-window_active");
     modalresizer();
     $("#" + artic).animate({
       opacity: 1
     }, 500);
-    $(".modal-shadow").addClass("js-active");
+    $(".modal-shadow").addClass("modal-window_active");
     $(".modal-shadow").animate({
       opacity: 0.8
     }, 500);
@@ -44,7 +44,7 @@ function closemodal() {
   $(".modal-window, .modal-shadow").animate({
     opacity: 0
   }, 500, function () {
-    $(".modal-window, .modal-shadow").removeClass("js-active");
+    $(".modal-window, .modal-shadow").removeClass("modal-window_active");
   });
 }
 
@@ -53,16 +53,16 @@ $(document).ready(function () {
   $("body").prepend("<div class='modal-shadow'></div>");
 
   $(".modal-window").each(function () {
-    $(this).prepend("<a href='' class='closemodal'></a>");
+    $(this).prepend("<a href='' class='modal-window__close-modal'></a>");
   });
 
-  $("body").on("click", ".closemodal, .modal-shadow", function (e) {
+  $("body").on("click", ".modal-window__close-modal, .modal-shadow", function (e) {
     closemodal();
     e.preventDefault();
   });
   
   $(window).resize(function() {
-    if ($(".modal-window.js-active").length > 0) {
+    if ($(".modal-window.modal-window_active").length > 0) {
       modalresizer();
     }
   });
